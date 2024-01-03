34e97e65de4c1cd358a34c498673b5335363aba8
Swati Sharma <103050981+swati-sharma14@users.noreply.github.com>
2024-01-03 19:55:55 +0530

    Create 1679 B - Stone Age Problem.cpp

---
+++ 1200/1679 B - Stone Age Problem.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int 
+
+int main(){
+    ll t, n;
+    cin >> t >> n;
+
+    vector<ll> nums(n);
+    ll ans = 0;
+    
+    for(ll i=0; i<n; i++){
+        cin >> nums[i];
+        ans += nums[i];
+    }
+    
+    map<ll,ll> mp;
+    ll curr = -1;
+    
+    for(ll i=0; i<t; i++){
+        ll a;
+        cin >> a;
+
+        if(a==1){
+            ll index, num;
+            cin >> index >> num;
+            
+            if(curr == -1){
+                ans -= nums[index-1];
+                ans += num;
+                nums[index - 1] = num;
+            }
+            else{
+                if(mp[index - 1] == 0){
+                    ans -= curr;
+                    ans += num;
+                    mp[index - 1] = num;
+                }
+                else{
+                    ans -= mp[index-1];
+                    ans += num;
+                    mp[index - 1] = num;
+                }
+            }
+        }
+        else{
+            ll num;
+            cin >> num;
+            
+            ans = (n*num);
+            mp.clear();
+            curr = num;
+        }
+        
+        cout << ans << endl;
+    }
+}
