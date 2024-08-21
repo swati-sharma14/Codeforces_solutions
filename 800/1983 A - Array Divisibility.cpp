d59a17000544b316f7759e6ab827bdec14e904c1
Swati Sharma <103050981+swati-sharma14@users.noreply.github.com>
2024-08-21 14:46:06 +0530

    Create 1983 A - Array Divisibility.cpp

---
+++ 800/1983 A - Array Divisibility.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ll t;
+    cin >> t;
+    
+    for(ll i=0; i<t; i++){
+        ll n;
+        cin >> n;
+        
+        for(ll j=1; j<=n; j++) cout << j << " ";
+        cout << endl;
+    }
+}
