bbeb9a379e4143cabdc8d7cf49d4e3981f3c6e4a
Swati Sharma <103050981+swati-sharma14@users.noreply.github.com>
2024-09-01 22:36:48 +0530

    Create 2008 A - Sakurako's Exam.cpp

---
+++ 800/2008 A - Sakurako's Exam.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+using namespace std;
+
+#define ll long long
+
+int main() {
+    ll t;
+    cin >> t;
+    while(t--){
+        ll a, b;
+        cin >> a >> b;
+        if(b%2 && a%2) cout << "NO" << endl;
+        else if(b%2 && !a%2) cout << "NO" << endl;
+        else if(a%2 && !b%2) cout << "NO" << endl;
+        else cout << "YES" << endl;
+    }
+}
